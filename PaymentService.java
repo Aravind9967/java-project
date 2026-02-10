ed7dd0e7ba3925c230e4d8ca805bfc5eb429ecab
Aravind9967 <babbadiaravind@gmail.com>
2026-02-10 20:22:16 +0530

    Delete PaymentService.java

--- PaymentService.java
+++
@@ -1,9 +0,0 @@
-package models;
-
-public class PaymentService {
-	public void pay(double amount) {
-		System.out.println("Processing payment of $ " + amount);
-		System.out.println("Payment successful. Thanks you");
-	}
-
-}

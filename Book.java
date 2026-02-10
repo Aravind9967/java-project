4099365a97a3b94e7a5d6fa33c5195c7f550ac02
Aravind9967 <babbadiaravind@gmail.com>
2026-02-10 20:19:05 +0530

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,32 @@
+package models;
+
+public class Book {
+	
+	private String title;
+	private Double price;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public Book(String title, Double price) {
+		super();
+		this.title = title;
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", price=" + price + "]";
+	}
+	
+	
+	
+
+}
---
+++ User.java
@@ -0,0 +1,24 @@
+package models;
+
+public class User {
+	private String username;
+	private String password;
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public User(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+	
+	
+	
+
+}
